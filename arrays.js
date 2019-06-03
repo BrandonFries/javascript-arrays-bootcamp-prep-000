9de1ea8d4a2824ae9495b2bf14f834375f12d541
Brandon Fries <brandonfries1993@gmail.com>
2019-06-03 18:28:11 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -11,6 +11,7 @@ function addElementToBeginningOfArray(array, element) {
 function descructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element);
   return array;
+}
 function destructivelyAddElementToBeginningOfArray(array, element) {
   array.unshift(element)
   return array
