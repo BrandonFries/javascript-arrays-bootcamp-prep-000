0bcd30ef3749ab508c7a7f21613a18fe25971871
Brandon Fries <brandonfries1993@gmail.com>
2019-06-03 17:34:32 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -1,8 +1,8 @@
-var 'chocolateBars' = [
+var chocolateBars = [
   'snikcers',
   'hundred grand',
   'kitkat',
   'skittles'
 ]
-var 'addElementToBeginningOfArray' = [array, element]
+var addElementToBeginningOfArray = [array, element]
 'addElementToBeginningOfArray'.push('element')
